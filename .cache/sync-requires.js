e0b9e472dacdee9ef5c22fcdac70ad346410d601
Z <asterixandisaac@gmail.com>
2019-10-20 00:11:36 -0600

    things

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,9 +5,7 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ztai/Downloads/Code/boostly-coupon/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/ztai/Downloads/Code/boostly-coupon/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/ztai/Downloads/Code/boostly-coupon/src/pages/index.js"))),
-  "component---src-pages-redeem-js": hot(preferDefault(require("/Users/ztai/Downloads/Code/boostly-coupon/src/pages/redeem.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ztai/Downloads/Code/personalWebsite/.cache/dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/ztai/Downloads/Code/personalWebsite/src/pages/index.js")))
 }
 
